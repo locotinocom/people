dc1a1b7ee13f6a5397382643cb7872553f818905
Umit Derin <uemitderin@gmail.com>
2026-03-31 10:40:32 +0200

    Fix: Path aliases for Vercel build

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,25 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react-swc"
 import tailwindcss from "@tailwindcss/vite"
 
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-   alias: {
-    "@helpers": "/src/helpers",
-    "@components": "/src/components",
-    "@data": "/src/data",
-    "@context": "/src/context",
-    "@": "/src",
-    "@store": "/src/store",
-"@layouts": "/src/layouts",
-    "@api": "/src/api",
-    "@game": "/src/components/game",
-    "@hooks": "/src/hooks",
-    "@utils": "/src/utils",
-    "@ui": "/src/ui",
-    "@types": "/src/types",
-     "@tools": "/src/tools",
-    
-
-  },
+    alias: {
+      "@": "/src",
+      "@helpers": "/src/helpers",
+      "@components": "/src/components",
+      "@data": "/src/data",
+      "@context": "/src/context",
+      "@store": "/src/store",
+      "@layouts": "/src/layouts",
+      "@api": "/src/api",
+      "@game": "/src/components/game",
+      "@hooks": "/src/hooks",
+      "@utils": "/src/utils",
+      "@ui": "/src/ui",
+      "@types": "/src/types",
+      "@tools": "/src/tools",
+    },
   },
 })
